d82e3ccc119022aac04b117b8042e9ce753eafa4
Razhan Hameed <razhan13@gmail.com>
2024-02-15 21:25:48 +0300

    spelling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,10 +11,10 @@ export default function Hero() {
     <div className="my-6 flex flex-col items-center gap-x-12 xl:mb-12 xl:flex-row">
       <div className="max-w-2xl pt-6">
         <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-          Hi, Razhan Hameed
+          Hi, I am Razhan Hameed
         </h1>
         <h2 className="prose text-lg text-gray-600 dark:text-gray-400">
-          {`I am Machine Learning Engineer. I will share my expereince here.`}
+          {`I am a Machine Learning Engineer. I will share my journey here.`}
         </h2>
       </div>
       <AvatarLayout content={mainContent}>
